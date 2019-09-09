4b3c4f9ee78bd9df834f6c78cef4be9ec0a94833
shenyc <shenyc@yiche.com>
2019-09-09 17:36:51 +0800

    first commit

---
+++ df_user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=30)
+    ushou = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length=100)
+    uyoubian = models.CharField(max_length=6)
+    uphone = models.CharField(max_length=11)
